8884f18fef9e3d664bd0507676e138678f0f249d
Prince jangra <132079316+Princejangra146@users.noreply.github.com>
2025-11-25 10:45:10 +0530

    fix: update import path for checkout component and fix case sensitivity

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import ProductDetails from './pages/productDetails';
-import Checkout from './pages/checkout';
+import Checkout from './pages/checkout.tsx';
 import OrderConfirmation from './pages/OrderConfirmation';
 import './index.css';
 
